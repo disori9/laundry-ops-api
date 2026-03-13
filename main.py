8c6e21f86db8ff751466ff1e973adade5dbc3097
Ori <disori777@gmail.com>
2026-03-13 10:06:17 +0800

    Add create order

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 import sqlite3
-from schemas import CustomerCreate
+import math
+from schemas import CustomerCreate, OrderCreate
 
 app = FastAPI()
 
@@ -26,3 +27,24 @@ def create_customer(customer: CustomerCreate):
 
     return {"message": "Customer created successfully", "name": customer.cust_name, "number": customer.number}
 
+
+@app.post("/orders")
+def create_order(order: OrderCreate):
+    kg_per_order = 6
+    load = math.ceil(order.weight_kg / kg_per_order)
+    total_price = 165 * load
+
+    conn = sqlite3.connect('laundry.db')
+    
+    cursor = conn.cursor()
+
+    command = 'INSERT INTO orders(weight_kg, total_price, payment_status, status, customer_id) VALUES (?, ?, ?, ?, ?)'
+    data_to_insert = (order.weight_kg, total_price, order.payment_status, 'RECEIVED', order.customer_id)
+
+    cursor.execute(command, data_to_insert)
+
+    conn.commit()
+
+    conn.close()
+
+    return {"message": f"Order created successfully, total price: {total_price}"}
--- schemas.py
+++ schemas.py
@@ -1,6 +1,13 @@
 from pydantic import BaseModel
+from typing import Literal
 
 
 class CustomerCreate(BaseModel):
     cust_name: str
     number: str
+
+
+class OrderCreate(BaseModel):
+    customer_id: int
+    weight_kg: float
+    payment_status: Literal['PAID', 'UNPAID']
