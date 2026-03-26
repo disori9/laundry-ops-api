b7a5230b3523207345db23069b36c9b6ea62cd2f
serefel <violetori99@gmail.com>
2026-03-26 09:24:13 +0800

    Move folder structure

---
+++ backend/init_db.py
@@ -0,0 +1,62 @@
+import sqlite3
+
+conn = sqlite3.connect('laundry.db')
+
+cursor = conn.cursor()
+
+cursor.execute('''
+        CREATE TABLE customers (
+            customer_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            cust_name TEXT NOT NULL,
+            number TEXT NOT NULL
+        );
+''')
+
+cursor.execute('''
+        CREATE TABLE orders (
+            order_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            weight_kg REAL NOT NULL,
+            total_price INT NOT NULL,
+            payment_status TEXT CHECK(payment_status IN ('PAID', 'UNPAID')),
+            customer_id INTEGER,
+            total_loads INTEGER NOT NULL,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            FOREIGN KEY (customer_id) REFERENCES customers (customer_id)
+                ON DELETE SET NULL
+        );
+''')
+
+cursor.execute('''
+        CREATE TABLE order_loads (
+               load_id INTEGER PRIMARY KEY AUTOINCREMENT,
+               order_id INTEGER,
+               status TEXT CHECK(status IN ('RECEIVED', 'WASHING', 'DRYING', 'FOLDING', 'BAGGED', 'COMPLETED')),
+               machine_no INTEGER,
+               FOREIGN KEY (order_id) REFERENCES orders (order_id)
+                       ON DELETE CASCADE)
+               ''')
+
+cursor.execute('''
+        CREATE TABLE item_categories (
+            category_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            category_name TEXT NOT NULL UNIQUE
+        );
+''')
+
+cursor.execute('''
+        CREATE TABLE order_items (
+            items_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            category_id INTEGER,
+            initial_count INTEGER NOT NULL,
+            verified_count INTEGER DEFAULT 0,
+            order_id INTEGER,
+            FOREIGN KEY (order_id) REFERENCES orders (order_id) ON DELETE CASCADE,
+            FOREIGN KEY (category_id) REFERENCES item_categories (category_id)
+        );
+''')
+
+conn.commit()
+
+conn.close()
+
+print("Database and table created successfully!")
---
+++ backend/schemas.py
@@ -0,0 +1,33 @@
+from pydantic import BaseModel
+from typing import Literal, Optional
+
+
+class CustomerCreate(BaseModel):
+    cust_name: str
+    number: str
+
+
+class OrderCreate(BaseModel):
+    customer_id: int
+    weight_kg: float
+    payment_status: Literal['PAID', 'UNPAID']
+    comforter_count: int=0
+
+
+class OrderItemCreate(BaseModel):
+    order_id: int
+    category_id: int
+    initial_count: int
+
+
+class LoadStatusUpdate(BaseModel):
+    status: Literal['RECEIVED', 'WASHING', 'DRYING', 'FOLDING', 'BAGGED', 'COMPLETED']
+    machine_no: Optional[int] = None
+
+
+class ItemVerification(BaseModel):
+    verified_count: int
+    
+    
+class PaymentUpdate(BaseModel):
+    payment_status: str
